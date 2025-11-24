e0ff9d07853a180078276591aa5875077d98b4d6
iredstonne <iredstonne.dev@gmail.com>
2025-11-24 19:24:50 +0100

    fix(player): Suppression du setLink inutilisé, réorganisation des méthodes, ajout de setX, setY sans implementation et remplacement des variables de coordonées par leur getter

--- src/Joueur.java
+++ src/Joueur.java
@@ -11,24 +11,22 @@ import javafx.scene.shape.Rectangle;
 
 public class Joueur {
 
-    private final double width =80;
-    private final double height=20;
+    private final double width = 80;
+    private final double height = 20;
 
     private int vitesse = 15;
 
     private double x = 400;
     private double y = 400;
 
+    private Group link;
 
     public Joueur(){
         this.link = createToken(1.0);
     }
 
-    private Group link;
-
     private Group createToken(double scale) {
-
-        Ellipse corp= new Ellipse(0,0, width/2, height/2);
+        Ellipse corp = new Ellipse(0,0, width/2, height/2);
         Circle tete = new Circle(0,0,15);
         Circle main = new Circle(32,-10,6);
         Rectangle sword = new Rectangle(25,-13 ,15,3);
@@ -38,10 +36,12 @@ public class Joueur {
         main.setFill(Color.DARKGREEN);
         sword.setFill(Color.WHITE);
 
-
         Group g = new Group(corp, tete, main, sword);
-
         return g;
+    }
+
+    public void spawn(Pane world) {
+        world.getChildren().add(this.link);
     }
 
     public void move(double movex, double movey, List<Rectangle> obstacles) {
@@ -52,7 +52,22 @@ public class Joueur {
 
     }
 
-    //add setter for angle
+    public void setX(double x) {
+        // TODO: Set layout x
+    }
+
+    public double getX() {
+        return this.link.getLayoutX();
+    }
+
+    public void setY(double y) {
+        // TODO: Set layout y
+    }
+
+    public double getY() {
+        return this.link.getLayoutY();
+    }
+
     public void setAngle(double angle) {
         this.link.setRotate(angle);
     }
@@ -61,98 +76,66 @@ public class Joueur {
         return this.link.getRotate();
     }
 
-
-    public double getX() {
-        return this.link.getLayoutX();
-    }
-
-    public double getY() {
-        return this.link.getLayoutY();
-    }
-
-
     public int getVitesse(){
         return this.vitesse;
     }
 
-
-
-
-    public void spawn(Pane world) {
-        this.link.setLayoutX(this.x);
-        this.link.setLayoutY(this.y);
-        world.getChildren().add(this.link);
-    }
-
     private void resolveCollisionOnXAxis(List<Rectangle> obstacles) {
+        double x = this.getX();
         for(final Rectangle obstacle : obstacles) {
             final double obstacleLeft = obstacle.getX();
             final double obstacleRight = obstacle.getX() + obstacle.getWidth();
             final double halfWidth = this.width / 2.0;
             if(this.canCollideLeft(obstacle)) {
-                this.x = obstacleLeft - halfWidth;
+                x = obstacleLeft - halfWidth;
             } else if(this.canCollideRight(obstacle)) {
-                this.x = obstacleRight + halfWidth;
+                x = obstacleRight + halfWidth;
             }
         }
+        this.setX(x);
     }
 
     private void resolveCollisionOnYAxis(List<Rectangle> obstacles) {
+        double y = this.getY();
         for(final Rectangle obstacle : obstacles) {
             final double obstacleTop = obstacle.getY();
             final double obstacleBottom = obstacle.getY() + obstacle.getHeight();
             final double halfHeight = this.height / 2.0;
             if(this.canCollideTop(obstacle)) {
-                this.y = obstacleTop - halfHeight;
+                y = obstacleTop - halfHeight;
             } else if(this.canCollideBottom(obstacle)) {
-                this.y = obstacleBottom + halfHeight;
+                y = obstacleBottom + halfHeight;
             }
         }
+        this.setY(y);
     }
 
     public boolean canCollideLeft(Rectangle rectangle) {
-        double xGaucheToken = this.x - (this.width/2);
+        double xGaucheToken = this.getX() - (this.width / 2);
         double murDroit  = rectangle.getX() + rectangle.getWidth();
 
         return (xGaucheToken <= murDroit && xGaucheToken >= rectangle.getX());
     }
 
     public boolean canCollideRight(Rectangle rectangle) {
-        double xDroiteToken = this.x + (this.width/2);
+        double xDroiteToken = this.getX() + (this.width / 2);
         double murGauche = rectangle.getX();
 
         return (xDroiteToken >= murGauche && xDroiteToken <= rectangle.getX());
     }
 
     public boolean canCollideTop(Rectangle rectangle) {
-        double yTopToken = this.y - (this.height/2);
+        double yTopToken = this.getY() - (this.height / 2);
         double murTop = rectangle.getY();
 
         return (yTopToken <= murTop && yTopToken >= rectangle.getY());
     }
 
     public boolean canCollideBottom(Rectangle rectangle) {
-        double yBottomToken = this.y + (this.y/2);
+        double yBottomToken = this.getY() + (this.y / 2);
         double murBottom = rectangle.getY() + rectangle.getHeight();
 
         return (yBottomToken >= murBottom && yBottomToken <= rectangle.getHeight());
     }
 
-    public void setLink(Group link) {
-        this.link = link;
-    }
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
+}
