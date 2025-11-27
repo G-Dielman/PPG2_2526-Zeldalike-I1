9496bca348c9ff7db405c350bb47c4edc53d81b2
iredstonne <iredstonne.dev@gmail.com>
2025-11-27 19:49:03 +0100

    fix(game object): Changement de type de retour de getToken

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,9 +1,9 @@
-import javafx.scene.Node;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 
 public abstract class GameObject {
 
-    protected abstract Node getToken();
+    protected abstract Group getToken();
 
     protected void addToPane(Pane pane) {
         pane.getChildren().add(this.getToken());
